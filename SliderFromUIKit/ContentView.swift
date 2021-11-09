d534755ef8ea1790a54d593f0d5ed95d217ebd40
Igor Buzykin <lexemz@gmail.com>
2021-11-09 22:05:18 +0300

    Create label and shuffle button

--- SliderFromUIKit/ContentView.swift
+++ SliderFromUIKit/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var desiredValue = Int.random(in: 1...100)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Установите слайдер к \(desiredValue)")
+
+            Button(action: shuffleDesiredValue) {
+                Text("Перемешать число")
+            }
+                
+        }
+        .padding()
+    }
+
+    private func shuffleDesiredValue() {
+        desiredValue = Int.random(in: 1...100)
     }
 }
 
