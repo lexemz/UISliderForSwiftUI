690594021d353fdd562ee8c7c49fdad4d7cbff1e
Igor Buzykin <lexemz@gmail.com>
2021-11-09 23:49:09 +0300

    Game is work

--- SliderFromUIKit/ContentView.swift
+++ SliderFromUIKit/ContentView.swift
@@ -8,22 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var desiredValue = Int.random(in: 1...100)
+    @State private var targetValue = Int.random(in: 1...100)
+    @State private var currentValue = Float.random(in: 1...100)
+    
+    private var score: Int {
+        let difference = abs(targetValue - lround(Double(currentValue)))
+        return 100 - difference
+    }
+    
+    @State private var alertPresented = false
 
     var body: some View {
         VStack {
-            Text("Установите слайдер к \(desiredValue)")
+            Text("\(currentValue)")
+            Text("Установите слайдер к \(targetValue)")
 
             Button(action: shuffleDesiredValue) {
-                Text("Перемешать число")
+                Text("Изменить число")
             }
-                
+            
+            
+            HStack {
+                Text("0")
+                CustomSlider(value: $currentValue)
+                Text("100")
+            }
+            
+            Button("Результат", action: resultPressed)
+            .alert("Результат", isPresented: $alertPresented, actions: {}) {
+                    Text("\(score)")
+                }
         }
         .padding()
     }
 
     private func shuffleDesiredValue() {
-        desiredValue = Int.random(in: 1...100)
+        targetValue = Int.random(in: 1...100)
+        currentValue = Float.random(in: 1...100)
+    }
+    
+    private func resultPressed() {
+        alertPresented = true
     }
 }
 
