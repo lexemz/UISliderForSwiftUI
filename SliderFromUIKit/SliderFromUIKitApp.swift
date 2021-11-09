ca0d738dd9a8f7e0cf9772161b2c66fe6cbf5462
Igor Buzykin <lexemz@gmail.com>
2021-11-09 21:36:51 +0300

    Initial Commit

---
+++ SliderFromUIKit/SliderFromUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderFromUIKitApp.swift
+//  SliderFromUIKit
+//
+//  Created by Igor on 09.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderFromUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
